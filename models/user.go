e5b81630f076128a21f4ec8fb22e4dffe949e215
David whyte <kingstonwhyte87@gmail.com>
2019-06-02 22:51:09 +0100

    Initialize project

--- models/user.go
+++ models/user.go
@@ -6,6 +6,9 @@ import "gopkg.in/mgo.v2/bson"
 type User struct {
 	ID          bson.ObjectId `bson:"_id" json:"id"`
 	Name        string        `bson:"name" json:"name" validate:"required,alpha"`
+	Email string  `bson:"email" json:"email" validate:"required,email"`
 	CoverImage  string        `bson:"cover_image" json:"cover_image"`
-	Bio string        `bson:"description" json:"description" validate:"required,alpha"`
+	Bio string        `bson:"bio" json:"bio" validate:"required,alpha"`
+	Location string `bson:"location" json:"location" validate:"required,alpha"`
+
 }
